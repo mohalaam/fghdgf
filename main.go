230d3fe7ac783184e1d7300f1dace12405942c19
mohalaam <120230216+mohalaam@users.noreply.github.com>
2024-11-15 21:51:32 +0100

    Update main.go

--- main.go
+++ main.go
@@ -24,5 +24,5 @@ func HelloHandler(w http.ResponseWriter, _ *http.Request) {
 	fmt.Fprintf(w, "{
   "Description": "Fichier de vérification de la propriété du domaine pour Microsoft 365 : placer dans la racine du site web",
   "Domain": "inappropriate-mahalia-csdfzs-6fb1f4b8.koyeb.app",
-  "Id": "f58a1e05-6512-4491-9510-7b16670249fe""}""")
+  "Id": "f58a1e05-6512-4491-9510-7b16670249fe")
 }
